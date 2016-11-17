8cb9b5720e899a4488ee67f304c5c40ec965fe02
U-ADS\embrande <embrande@IN-STSV-25JNJB2.ads.iu.edu>
2016-11-17 16:22:48 -0500

    Added files. Continued on header function

--- _Assets/functions/functions_classes.php
+++ _Assets/functions/functions_classes.php
@@ -1,5 +1,14 @@
 <?php
 
+	function register_javascript_header($name, $file_path, $dependant_name){
+
+		$javascript_file = new register_javascript_header;
+
+		$javascript_file->set_variables($name, $file_path, $dependant_name);
+		$javascript_file->add_to_javascript_array;
+
+	}
+
 
 	class class_register_javascript_header
 	{
@@ -8,9 +17,18 @@
 		var $file_path;
 		var $dependant_name;
 
-		function set_variables(){
+		function set_variables($name, $file_path, $dependant_name){
 
+			$this->name = $name;
+			$this->file_path = $file_path;
+			$this->dependant_name = $dependant_name;
 			
+		}
+
+		function add_to_javascript_array(){
+
+			sort_registered_javascript();
+
 		}
 
 		function set_file_path(){
