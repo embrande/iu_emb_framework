8e827f1c79fe8ac8a7d86c11a7f2d92d0a52a78f
U-ADS\embrande <embrande@IN-STSV-25JNJB2.ads.iu.edu>
2016-12-05 16:23:27 -0500

    Added functionality to classes

--- _Assets/functions/functions.php
+++ _Assets/functions/functions.php
@@ -14,20 +14,22 @@
 
 	/*
 		-Get and register header javascript and css specific folders
-		-Function expects (name,path[use variable $jsPath to reference js folder] / file name,name of dependant on)
+		-Function expects (name,path[use variable $jsPath to reference js folder] / file name,name of dependant on, false if not a local file)
 	*/
 	function function_register_javascript_header(){
 
 		register_javascript_header(
 			'jquery',
 			'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js',
-			''
+			'',
+			'false'
 		);
 
 		register_javascript_header(
 			'name',
-			$jsPath . 'global.js',
-			'jquery'
+			'global.js',
+			'jquery',
+			''
 		);
 
 	}
@@ -40,6 +42,7 @@
 	/*
 		Executes functions from array
 	*/
+	function_register_javascript_header();
 	execute_functions();
 
 ?>
--- _Assets/functions/functions_classes.php
+++ _Assets/functions/functions_classes.php
@@ -1,10 +1,16 @@
 <?php
 
-	function register_javascript_header($name, $file_path, $dependant_name){
+	/*
+		Global variables used in classes
+	*/
+	$GLOBALS['FUN_QUEUE'] = [];
 
-		$javascript_file = new register_javascript_header;
 
-		$javascript_file->set_variables($name, $file_path, $dependant_name);
+	function register_javascript_header($name, $file_path, $dependant_name, $local){
+
+		$javascript_file = new class_register_javascript_header;
+
+		$javascript_file->set_variables($name, $file_path, $dependant_name, $local);
 
 	}
 
@@ -17,15 +23,21 @@
 		var $dependant_name;
 		var $header_javascript_array;
 
-		function set_variables($name, $file_path, $dependant_name){
+		public function set_variables($name, $file_path, $dependant_name, $local){
+
+			if( ($local == 'false') || ($this == '') ){
+				$this->local = $local;
+			}else{
+				$this->local = 'true';
+			}
 
 			$this->name = $name;
 			$this->file_path = $file_path;
 			$this->dependant_name = $dependant_name;
-			$this->header_javascript_array = $header_javascript_array;
+			$this->header_javascript_array = '';
 
-			set_file_path();
-			sort_registered_javascript();
+			$this->set_file_path();
+			//sort_registered_javascript();
 			
 		}
 
@@ -33,9 +45,16 @@
 
 		}
 
-		function set_file_path(){
+		public function set_file_path(){
 
 			//get the file path, and based off of a php value set it to the js folder and concatinate it to what the array the user sent
+			global $JAVASCRIPT_FILE_PATH;
+
+			if($this->local == 'true'){
+				$this->file_path = $JAVASCRIPT_FILE_PATH . $this->file_path;
+			}else{
+				$this->file_path = $this->file_path;
+			}
 
 		}
 
@@ -51,26 +70,37 @@
 			//sort array and compare to global array to see if it's already in it by name.
 			//then, based on dependencies, place it into variable. If none, place at end.
 
-			global $HEADER_JAVASCRIPT;
+			global $FUN_QUEUE;
 
 			//place global array into a local variable
 			$this->header_javascript_array = $HEADER_JAVASCRIPT;
+
+			if(isset($GLOBALS['FUN_QUEUE']) && ($GLOBALS['FUN_QUEUE'] !== '')){
+
+				//If there are already values inside of the Global variable.
+				$utility_functions = $GLOBALS['FUN_QUEUE']; 
+
+			}else{
+
+				echo 'here';
+
+				//if there isn't, place at the end of the array
+				$GLOBALS['FUN_QUEUE'][] = ['script_name' => $script_name, 'script_path' => $script_path]; 
+
+			}
+
 			
 		}
 
 	}
 
 
+	
+
 	//enqueues functions
 	function enqueue_functions($name, $dependant_name){
 
-		if(isset($GLOBALS['UTILITY_FUNCTIONS'])){
-			$UTILITY_FUNCTIONS = $GLOBALS['UTILITY_FUNCTIONS']; 
-		}
-
-		if($dependant_name){
-			echo "eric";
-		}
+		//adds functions to queue 
 
 	}
 	//executes functions
