047e13708eda53d26e0912f2023a418a59d205b9
U-ADS\embrande <embrande@IN-STSV-25JNJB2.ads.iu.edu>
2017-05-08 09:44:07 -0400

    added

--- _Assets/functions/functions.php
+++ _Assets/functions/functions.php
@@ -1,6 +1,5 @@
 <?php 
     require_once("functions_classes.php");
-    echo $template_name;
     /* Global Variables:
  
         $HEADER_JAVASCRIPT
--- wwws/index.php
+++ wwws/index.php
@@ -2,10 +2,14 @@
 	
 	//DETERMINE THE TEMPLATE
 	//SET TITLE
+	$title = "home - index";
 	
-	$template_name = "index";
+	//set Template-name - defaults to default.php; Must have template- at the beginning
+	//then - template-parent-child.php
+	$template_name = "hello";
 
 	
+	//require document setup - this sets up the backbone to the page.
 	require_once ("setup.php");
 
 
